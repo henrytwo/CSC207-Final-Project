f522059627495f7d3fbb6add356a30d819587669
Henry T <henry@henrytu.me>
2020-11-07 14:16:36 -0500

    Notes for calendar

--- phase1/src/util/calendar/Calendar.java
+++ phase1/src/util/calendar/Calendar.java
@@ -2,4 +2,7 @@ package util.calendar;
 
 public class Calendar {
     // Look at the LocalDateTime library
+    // You can probably just store pairs of LocalDateTime (for start and end), in a set (Maybe make another class for the pair?)
+    // To check if a time slot is going to conflict, compare the new pair with the previous pairs
+    // We'll probably also want to be able to store the event UUID in the pair
 }
