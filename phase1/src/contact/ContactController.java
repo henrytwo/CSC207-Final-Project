aa1cf0fc19b9e2c46e83153b88b9e030ccc395dc
pranjal <pranjal.bjaraia@mail.utoronto.ca>
2020-11-08 18:35:08 +0530

    content class

--- phase1/src/contact/ContactController.java
+++ phase1/src/contact/ContactController.java
@@ -1,4 +1,26 @@
 package contact;
 
+import java.util.Set;
+import java.util.UUID;
+
 public class ContactController {
+    ContactManager hermes = new ContactManager();
+    public void sendRequest(UUID userId, UUID potentialContact){
+        Set<UUID> requestList = hermes.getRequests(potentialContact);
+        Set<UUID> sentList = hermes.getSentRequests(userId);
+        requestList.add(userId);
+        sentList.add(potentialContact);
+        hermes.setRequests(potentialContact, requestList);
+        hermes.setSentRequests(userId, sentList);
+    }
+
+    public void deleteContacts(UUID userId, UUID extraContact){
+        Set<UUID> contactsList = hermes.getContacts(userId);
+        contactsList.remove(extraContact);
+        hermes.setContacts(userId, contactsList);
+    }
+
+//    public Set<UUID> showContacts(){
+//    }
+//    ARE WE RETURNING USERS OR JUST THEIR IDS?
 }
