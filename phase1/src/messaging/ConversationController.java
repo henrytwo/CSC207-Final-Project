9dbdd55c080d63c4db7e311da3331c1c9ff03475
pranjal <pranjal.bjaraia@mail.utoronto.ca>
2020-11-09 22:38:14 +0530

    Made changes to the controller for contact.

--- phase1/src/messaging/ConversationController.java
+++ phase1/src/messaging/ConversationController.java
@@ -1,4 +1,12 @@
 package messaging;
 
+import contact.ContactController;
+
+import java.util.UUID;
+
 public class ConversationController {
+    ContactController linky = new ContactController();
+    public boolean checkAccess(UUID sender, UUID receiver){
+        return linky.showContacts(sender).contains(receiver);
+    }
 }
