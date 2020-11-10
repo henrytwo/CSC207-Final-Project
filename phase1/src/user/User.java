c6ae779051bbaa3f311afb0044b49a3b8b8843bb
Shubhra Bedi <shubhrabedi@gmail.com>
2020-11-10 19:27:00 +0300

    Updated User, UserController, UserManager

--- phase1/src/user/User.java
+++ phase1/src/user/User.java
@@ -1,27 +1,68 @@
 package user;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.UUID;
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class User {
     private String name;
     private String email;
-    List<User> user = new ArrayList<User>();
+    private String password;
+    // List<User> user = new ArrayList<User>();
+    private Set<UUID> userUUIDs = new HashSet<>();
+    private UUID uuid;
 
-    public void setName(String name){
+    public User(String name, String email, UUID userUUID) {
+        this.name = name;
+        this.email = email;
+        this.uuid = UUID.randomUUID();
+        this.userUUIDs.add(userUUID);
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
-    public void setEmail(String email){
+    public void setEmail(String email) {
         this.email = email;
     }
 
-    public String getEmail(){
+    public String getEmail() {
         return this.email;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
+//    public int getSimpleHash(String password){
+
+//    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    boolean isUser(UUID userUUID) {
+        return userUUIDs.contains(userUUID);
+    }
+
+    public Set<UUID> getUserUUIDs() {
+        return userUUIDs;
+    }
+
+    public void addUser(UUID userUUID) {
+        userUUIDs.add(userUUID);
     }
 
 }
--- phase1/src/user/UserManager.java
+++ phase1/src/user/UserManager.java
@@ -1,4 +1,33 @@
 package user;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserManager {
-}
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner (System.in);
+
+        String username;
+        String password;
+
+
+        System.out.println("Welcome to your Event booking site!");
+        System.out.println("\n Enter your username and password to login to your account.");
+
+        System.out.println("Username: ");
+        username = input.nextLine();
+
+        System.out.println("Password: ");
+        password = input.nextLine();
+
+        UserController login = new UserController(username, password);
+
+        if(login.isLoginCorrect(username, password))
+            System.out.println("You are logged in!");
+        else
+            System.out.println("The username and password you entered are incorrect.");
+    }
+
+    }
+
