a8d587e5e6fb6af5f534c7980319842b53b5b691
Henry T <henry@henrytu.me>
2020-11-12 09:32:23 -0500

    Rebased UI on updated convention code

--- phase1/src/ConventionSystem.java
+++ phase1/src/ConventionSystem.java
@@ -1,13 +1,10 @@
+import console.LoginAndRegisterUI;
+import console.MainMenuUI;
 import convention.ConferenceController;
 import convention.EventController;
 import convention.RoomController;
-import convention.calendar.TimeRange;
 import convention.conference.ConferenceManager;
-import convention.permission.PermissionManager;
 
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.util.UUID;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
@@ -31,7 +28,69 @@ public class ConventionSystem {
         EventController eventController = new EventController(conferenceManager);
         ConferenceController conferenceController = new ConferenceController(/*conversationController,*/ eventController, conferenceManager);
 
-        // Test stuff
+        /*
+         * User flow
+         *
+         * ** Make attendee operations executable by admins too
+         * ** So it'd be check if organizer OR (if target == self AND is attendee)
+         *
+         * /
+         * |-Login or Register                                           ** Ez clap menu thing
+         *   |- Go to messaging
+         *   |  |-Look at the list of messages                           ** Ez clap menu thing
+         *   |  | |-Select a conversation                                ** Custom message boi
+         *   |  |   |-Send message
+         *   |  |   |-Read message
+         *   |  |   |-See users in conversation
+         *   |  |-Start a new message with someone on your contact list
+         *   |
+         *   |- Go to contacts                                            ** Ez clap menu thing
+         *   |  |-View contacts                                           ** Custom stuff
+         *   |  |-Request someone to connect
+         *   |  |-View people who want to slide into your DMs
+         *   |
+         *   |- Go to conferences                                         ** Ez clap menu thing
+         *  |-> Create a conference                                   ** Form boi
+         *  |-> Join a conference                                     ** Ez clap menu thing
+         *  |   |-> Find a conference from a list and join it
+         *  |-> View a joined conference                              ** Ez clap menu thing
+         *      |-> View the general conference details (start, end, name, etc.)
+         *      |
+         *      |-> View the event calendar
+         *      |
+         *      |-> Event stuff
+         *      |   |-> View list of events (Attendee)
+         *      |   |-> View list of events (Speaker)
+         *      |   |-> View event room
+         *      |   |-> View event time stuff
+         *      |   |-> Register for event
+         *      |   |-> Unregister from event
+         *      |   |-> Make a conversation for everyone in this event (Speaker)
+         *      |   |-> View a list of attendees, speakers
+         *      |   |-> Organizer related operations
+         *      |       |-> Edit the name, dates, etc.
+         *      |       |-> Create event
+         *      |       |-> Delete event
+         *      |
+         *      |-> Room stuff
+         *      |   |-> View calendar
+         *      |   |-> Organizer related operations
+         *      |        |-> Edit the room capacity, location, etc.
+         *      |        |-> Create room
+         *      |        |-> Delete room
+         *      |
+         *      |-> Conference management stuff
+         *          |-> Delete the conference
+         *          |-> Slide into the DM of any attendee
+         *          |-> Add/Remove organizer
+         * */
+
+        /**
+         * UI Components to build
+         * - n-column table with numbered rows
+         * - messaging thing
+         * - Form component
+         */
 
         /**
          * Big thinking
@@ -39,36 +98,15 @@ public class ConventionSystem {
          * - Dealing with CompareBySpeaker
          * - Dealing with speaker conflicts
          */
+        LoginAndRegisterUI loginAndRegisterUI = new LoginAndRegisterUI(userController);
+        MainMenuUI mainMenuUI = new MainMenuUI();
 
-        LocalDateTime dateA = LocalDateTime.of(2015,
-                Month.JULY, 29, 19, 30, 40);
-        LocalDateTime dateB = LocalDateTime.of(2018,
-                Month.JULY, 29, 19, 30, 40);
-
-        UUID me = UUID.randomUUID();
-        UUID attendee1 = UUID.randomUUID();
-        UUID attendee2 = UUID.randomUUID();
-        UUID otherOrganizer = UUID.randomUUID();
-        UUID speaker1 = UUID.randomUUID();
-
-        UUID conference1 = conferenceController.createConference("My convention", new TimeRange(dateA, dateB), me);
-
-        for (UUID confUUID : conferenceController.getConferences()) {
-            System.out.println(conferenceController.getConferenceName(confUUID));
+        while (true) {
+            if (userController.getCurrentUser() == null) {
+                loginAndRegisterUI.run();
+            } else {
+                mainMenuUI.run();
+            }
         }
-
-        conferenceController.addOrganizer(conference1, me, otherOrganizer);
-
-        conferenceController.addAttendee(conference1, attendee1);
-        conferenceController.addAttendee(conference1, attendee2);
-
-        System.out.println(eventController.getEvents(conference1, attendee1));
-
-        System.out.println(conferenceController.getAttendees(conference1, otherOrganizer));
-        eventController.createEventConversation(conference1, me, me);
-        //System.out.println(conferenceController.getEvents(conference1, attendee1));
-
-
-        System.out.println(conferenceController.getAttendees(conference1, otherOrganizer));
     }
 }
