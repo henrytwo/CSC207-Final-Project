bc78515a0e42d89174d8d3995dc327f1c5061458
Henry T <henry@henrytu.me>
2020-11-12 12:07:43 -0500

    Return UUID for login too

--- phase1/src/user/UserController.java
+++ phase1/src/user/UserController.java
@@ -1,20 +1,18 @@
 package user;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 public class UserController {
 
     /**
      * Responsibilities:
-     *
+     * <p>
      * - Stores an instance of UserManager
      * - Handles login (i.e. tags in a username + password pair and returns whether or not the login was successful
-     *   - Saves a copy of the logged in user's UUID in UserController
-     *
+     * - Saves a copy of the logged in user's UUID in UserController
+     * <p>
      * - Getter for the logged in user's UUID
-     *
+     * <p>
      * - Handles logout (i.e. erases the UUID of the logged in user)
      * - Handles account registration (i.e. given a username + password pair, create a user)
      */
@@ -23,7 +21,8 @@ public class UserController {
     UserManager userManager = new UserManager();
 
     public void setUserFirstName(String firstName) {
-        userManager.setUserFirstName(currentUser, firstName);;
+        userManager.setUserFirstName(currentUser, firstName);
+        ;
     }
 
     public String getUserFirstName() {
@@ -46,18 +45,18 @@ public class UserController {
         return userManager.getUserUsername(currentUser);
     }
 
-    public void setUserPassword(String password){
+    public void setUserPassword(String password) {
         userManager.setUserPassword(currentUser, password);
     }
 
-    public  UUID registerUser(String firstName, String lastName, String username, String password){
+    public UUID registerUser(String firstName, String lastName, String username, String password) {
         currentUser = userManager.registerUser(firstName, lastName, username, password);
         return currentUser;
     }
 
-    public boolean login(String username, String password) {
-        currentUser = userManager.login(username, password) ;
-        return true;
+    public UUID login(String username, String password) {
+        currentUser = userManager.login(username, password);
+        return currentUser;
     }
 
     public UUID getCurrentUser() {
