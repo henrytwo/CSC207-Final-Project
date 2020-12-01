f43dfa5eb8e05caa1cd211419ac17eca107ce4de
Shubhra Bedi <shubhrabedi@gmail.com>
2020-12-02 00:09:20 +0300

    LoginView and LoginView.form done

--- phase2/src/gui/login/LoginView.java
+++ phase2/src/gui/login/LoginView.java
@@ -7,12 +7,9 @@ import javax.swing.*;
 
 public class LoginView extends JFrame implements IPanel, ILoginView {
     private JPanel panel;
-    //private JLabel user_label, password_label, message;
     private JButton loginButton;
-    private JButton godLoginButton;
-    /*private JTextField userName_text;
-    private JPasswordField password_text;
-    private JButton submit, cancel;*/
+    private JTextField username;
+    private JPasswordField password;
     private LoginPresenter loginPresenter;
 
     /**
@@ -24,39 +21,14 @@ public class LoginView extends JFrame implements IPanel, ILoginView {
         loginPresenter = new LoginPresenter(mainFrame, this);
 
         loginButton.addActionListener((e) -> loginPresenter.login());
-        godLoginButton.addActionListener((e) -> loginPresenter.loginAsGod());
     }
-    /*
-    LoginView() {
-        // Username Label
-        user_label = new JLabel();
-        user_label.setText("User Name :");
-        userName_text = new JTextField();
-        // Password Label
-        password_label = new JLabel();
-        password_label.setText("Password :");
-        password_text = new JPasswordField();
-        // Submit
-        submit = new JButton("SUBMIT");
-        panel = new JPanel(new GridLayout(3, 1));
-        panel.add(user_label);
-        panel.add(userName_text);
-        panel.add(password_label);
-        panel.add(password_text);
-        message = new JLabel();
-        panel.add(message);
-        panel.add(submit);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // Adding the listeners to components..
-        submit.addActionListener(this);
-        add(panel, BorderLayout.CENTER);
-        setTitle("Please Login Here !");
-        setSize(450,350);
-        setVisible(true);
+
+    public String getUsername() {
+        return username.getText();
     }
-    public static void main(String[] args) {
-        new LoginView();
-    }*/
+    public String getPassword() {
+        return String.valueOf(password.getPassword());
+    }
 
     @Override
     public JPanel getPanel() {
