db81b453c98c3633013f6ced3024c4e3aaf210a8
santaras <70446573+santaras@users.noreply.github.com>
2020-12-01 13:55:07 -0500

    Started Room gui

---
+++ phase2/src/gui/conference/room/ConferenceRoomView.java
@@ -0,0 +1,4 @@
+package gui.conference.room;
+
+public class ConferenceRoomView {
+}
---
+++ phase2/src/gui/conference/room/IConferenceRoomView.java
@@ -0,0 +1,6 @@
+package gui.conference.room;
+
+public interface IConferenceRoomView {
+    void setTableData(String[][] tableData, String[] columnNames);
+
+}
