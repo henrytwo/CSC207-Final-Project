1587b7381b5fa27f4dff4f2ea30f94aa0f52c763
emrealca <emre.alca@icloud.com>
2020-12-03 21:50:20 -0500

    fixing read/unread message

--- phase2/src/messaging/ConversationManager.java
+++ phase2/src/messaging/ConversationManager.java
@@ -203,7 +203,7 @@ public class ConversationManager implements Serializable {
                 messageAsHashmap.put("timestamp", message.getTimestamp().toString());
                 messageAsHashmap.put("content", message.getContent());
                 newList.add(messageAsHashmap);
-                message.setIsRead(true);
+                //message.userReadMessage(userUUID);
             }
             return newList;
         } else {
