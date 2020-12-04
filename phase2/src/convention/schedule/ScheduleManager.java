130d92297531c114ecd3b45efeff2f7700ee9f1d
Ellie <illie.zhang@mail.utoronto.ca>
2020-12-04 10:48:29 -0500

    downloadable schedule backend complete

--- phase2/src/convention/schedule/ScheduleManager.java
+++ phase2/src/convention/schedule/ScheduleManager.java
@@ -4,19 +4,35 @@ import convention.event.Event;
 import gateway.SchedulePrinter;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Set;
 
 public class ScheduleManager {
 
-//    TODO: implement this
-    public static Schedule constructSchedule(Set<Event> e, String sortBy) {
+    /**
+     * @param eventStringListsList list of list of strings, each sublist looks like
+     *                             [event title,
+     *                             event time range,
+     *                             room location,
+     *                             name of speakers]
+     * @param sortBy one of "speaker", "registered", or "day"
+     * @param titleInfo one of: speaker username, user username, a specified date
+     * @return a Schedule with given information
+     */
+    public static Schedule constructSchedule(ArrayList<ArrayList<String>> eventStringListsList, String sortBy, String titleInfo) {
         Schedule s = new Schedule();
-
+        if (sortBy.equals("speaker")) {
+            s.setTitle("Events with speaker ".concat(titleInfo));
+        }
+        else if (sortBy.equals("registered")) {
+            s.setTitle("Events ".concat(titleInfo).concat(" signed up for"));
+        }
+        else {s.setTitle("events on ".concat(titleInfo));}
+        s.setEventStringList(eventStringListsList);
         return s;
     }
 
-
-    public void print(Schedule s) throws IOException {
+    public static void print(Schedule s) throws IOException {
         SchedulePrinter.print(s.getEventStringLists(), s.getTitle());
     }
 }
