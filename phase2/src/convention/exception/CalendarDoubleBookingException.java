6fa940a6076f66c10bd6ed4a70191cb1cc7036a6
Henry T <henry@henrytu.me>
2020-12-03 23:08:31 -0500

    Verbose double booking message

--- phase2/src/convention/exception/CalendarDoubleBookingException.java
+++ phase2/src/convention/exception/CalendarDoubleBookingException.java
@@ -5,7 +5,7 @@ package convention.exception;
  */
 public class CalendarDoubleBookingException extends RuntimeException {
     public CalendarDoubleBookingException() {
-        super("The time slot given conflicts with an existing event. ");
+        super("The time slot given conflicts with an existing event in the same room.");
     }
 }
 
