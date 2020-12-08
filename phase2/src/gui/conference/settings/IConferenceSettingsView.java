edacf9392f591c964a3b5b6c592ddf51fe3a45f6
emrealca <emre.alca@icloud.com>
2020-12-07 20:10:47 -0500

    adding javadocs

--- phase2/src/gui/conference/settings/IConferenceSettingsView.java
+++ phase2/src/gui/conference/settings/IConferenceSettingsView.java
@@ -1,5 +1,8 @@
 package gui.conference.settings;
 
+/**
+ * interface for the conference settings view of the GUI
+ */
 public interface IConferenceSettingsView {
     void setUserList(String[][] tableData, String[] columnNames);
 }
